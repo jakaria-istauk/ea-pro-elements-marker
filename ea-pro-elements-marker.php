b8b50bf679797f4a2d661d9b5b366132ea3cefa8
Jakaria Istauk <jakariamd35@gmail.com>
2024-01-22 22:50:24 +0600

    dynamic css class added

--- ea-pro-elements-marker.php
+++ ea-pro-elements-marker.php
@@ -18,24 +18,25 @@
     add_action( 'admin_footer', function(){
         if( isset( $_GET['page'] ) && $_GET['page'] === 'eael-settings' ){
             $elements_set = get_option( '_eael_widget_lists_for_dashboard_identify' );
+            $class_name = "eael-pro-widget";
             if( is_array( $elements_set ) && !empty( $elements_set ) ){
                 echo "<script>";
                 foreach( $elements_set as $elements ){
                     foreach( $elements[ 'elements' ] as $element ){
                         if( isset( $element['is_pro'] ) && $element['is_pro'] ){
-                            echo "jQuery('#{$element['key']}').closest('.eael-element__item').addClass('eael-pro-widget');";
+                            echo "jQuery('#{$element['key']}').closest('.eael-element__item').addClass('{$class_name}');";
                         }
                     }
                 }
                 echo "</script>";
 
                 echo "<style>";
-                echo '.eael-pro-widget{
+                echo ".{$class_name}{
                         position: relative;
                         border: 1px solid #c36 !important;
                     }
-                    .eael-pro-widget:before{
-                        content: "PRO";
+                    .{$class_name}:before{
+                        content: 'PRO';
                         position: absolute;
                         top: 30%;
                         left: -9px;
@@ -46,7 +47,7 @@
                         padding: 2px;
                         border-radius: 3px;
                         font-weight: bold;
-                    }';
+                    }";
                 echo "</style>";
             }
         }
