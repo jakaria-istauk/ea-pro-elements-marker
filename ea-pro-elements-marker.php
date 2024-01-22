ef5dae3893a2ebaac08d26633615f5feeeb312b5
Jakaria Istauk <jakariamd35@gmail.com>
2024-01-22 22:39:32 +0600

    Script added for elements

--- ea-pro-elements-marker.php
+++ ea-pro-elements-marker.php
@@ -18,7 +18,17 @@
     add_action( 'admin_footer', function(){
         if( isset( $_GET['page'] ) && $_GET['page'] === 'eael-settings' ){
             $elements_set = get_option( '_eael_widget_lists_for_dashboard_identify' );
-            
+            if( is_array( $elements_set ) && !empty( $elements_set ) ){
+                echo "<script>";
+                foreach( $elements_set as $elements ){
+                    foreach( $elements[ 'elements' ] as $element ){
+                        if( isset( $element['is_pro'] ) && $element['is_pro'] ){
+                            echo "jQuery('#{$element['key']}').closest('.eael-element__item').addClass('eael-pro-widget');";
+                        }
+                    }
+                }
+                echo "</script>";
+            }
         }
     } );
 }
